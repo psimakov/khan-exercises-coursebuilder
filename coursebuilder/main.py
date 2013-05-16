8ffd524d8d5fcf214b548d7118f716688a57fbe4
Pavel Simakov <pavel@vokamis.com>
2013-05-15 17:13:00 -0700

    First version of Khan Academy Exercises for Course Builder.

---
+++ coursebuilder/main.py
@@ -0,0 +1,69 @@
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Course Builder web application entry point."""
+
+__author__ = 'Pavel Simakov (psimakov@google.com)'
+
+import os
+import webapp2
+
+# The following import is needed in order to add third-party libraries.
+import appengine_config  # pylint: disable-msg=unused-import
+
+from common import tags
+from controllers import sites
+from models import custom_modules
+
+import modules.admin.admin
+import modules.announcements.announcements
+import modules.courses.courses
+import modules.dashboard.dashboard
+import modules.khanex.khanex
+import modules.oeditor.oeditor
+import modules.review.review
+
+
+# use this flag to control debug only features
+debug = not appengine_config.PRODUCTION_MODE
+
+# init and enable all known modules
+modules.oeditor.oeditor.register_module().enable()
+modules.admin.admin.register_module().enable()
+modules.dashboard.dashboard.register_module().enable()
+modules.announcements.announcements.register_module().enable()
+modules.review.review.register_module().enable()
+modules.courses.courses.register_module().enable()
+modules.khanex.khanex.register_module().enable()
+
+# compute all possible routes
+global_routes, namespaced_routes = custom_modules.Registry.get_all_routes()
+
+# routes available at '/%namespace%/' context paths
+sites.ApplicationRequestHandler.bind(namespaced_routes)
+app_routes = [(r'(.*)', sites.ApplicationRequestHandler)]
+
+# tag extension resource routes
+extensions_tag_resource_routes = [(
+    '/extensions/tags/.*/resources/.*', tags.ResourcesHandler)]
+
+# i18n configuration for jinja2
+webapp2_i18n_config = {'translations_path': os.path.join(
+    appengine_config.BUNDLE_ROOT, 'modules/i18n/resources/locale')}
+
+# init application
+app = webapp2.WSGIApplication(
+    global_routes + extensions_tag_resource_routes + app_routes,
+    config={'webapp2_extras.i18n': webapp2_i18n_config},
+    debug=debug)
